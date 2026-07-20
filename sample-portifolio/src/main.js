4424da3a24e2d3827e5c4f0e707386bf0f69a387
Cleyton Durans <Cleytondurans.n@gmail.com>
2026-07-20 12:11:19 -0300

    Incluindo HTML

--- sample-portifolio/src/main.js
+++ sample-portifolio/src/main.js
@@ -1,7 +1,1 @@
-import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
-
-
+import './style.css' 
